d5ab52720bd9fbfd675188729f99fe0d43961954
Admin <admin@Admins-MacBook-Pro.local>
2022-09-08 07:29:58 +0100

    Programming DHT11 via MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include<DHT.h>
+#include<DHT_U.h>
+#include <Adafruit_Sensor.h>
 
 
 const char* ssid ="TP-LINK_9512B5";
@@ -12,9 +15,22 @@ const char* mqtt_server = "broker.hivemq.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-int ledPin =4 ;
-const int ledPin2=19;
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
+
+#define DHTPIN 26     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+int ledPin =4 ;
 
 
 void setupWifi(){
@@ -28,26 +44,12 @@ void setupWifi(){
     delay(100);
     Serial.print("-");
   }
-  Serial.print("\nConnected to");
-  Serial.println(ssid);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
-void reconnect(){
-  while(!client.connected()){
-    Serial.print("\nConnecting to ");
-    Serial.println(mqtt_server);
-    if(client.connect("clientId-GCSG0K2cTB")){
-      Serial.print("\nConnected to ");
-      Serial.println(mqtt_server);
-      client.subscribe("esp32/LED");
-      client.subscribe("iot");
-      
-    }
-    else{
-      Serial.println("\nTrying connect again");
-      delay(5000);
-    }
-  }
-}
+
 
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
@@ -65,7 +67,7 @@ void callback(char* topic, byte* message, unsigned int length) {
 
   // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
   // Changes the output state according to the message
-  if (String(topic) == "esp32/LED") {
+  if (String(topic) == "esp32/output") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
       Serial.println("on");
@@ -78,22 +80,37 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
 }
 
+void reconnect(){
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 
 void setup() {
 
   // put your setup code here, to run once:
   Serial.begin(115200);
+  dht.begin();
   
   setupWifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   pinMode(ledPin,OUTPUT);
-  pinMode(ledPin2,OUTPUT);
-  
-  reconnect();
 }
-
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -101,5 +118,31 @@ if(!client.connected()){
   reconnect();
 }
 client.loop();
+long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Temperature in Celsius
+    float temperature = dht.readTemperature();   
+    // Uncomment the next line to set temperature in Fahrenheit 
+    // (and comment the previous temperature line)
+    //temperature = 1.8 * bme.readTemperature() + 32; // Temperature in Fahrenheit
+    
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    client.publish("esp32/temperature1", tempString);
 
+    float humidity = dht.readHumidity();
+    
+    
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    client.publish("esp32/humidity1", humString);
+  }
 }
